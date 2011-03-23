1768c97b52143e4d52eebf4e7b522db356afc4aa
Marco Munizaga <Marco.emunizaga@gmail.com>
2011-03-23 08:32:00 -0400

    cleaned up the director a bit, donnie I moved your files to an algorithm package in GSF/src/algorithm

---
+++ GSF/src/algorithm/Person.java
@@ -0,0 +1,29 @@
+package algorithm;
+
+public class Person{
+	private Location currentLocation;
+	private Location velocity;
+	//velocity is actually a Location object, 
+	//but it's "Location" is denoted as units of change of location per one second
+	
+	public Person(Location loc){
+		currentLocation = loc;
+		velocity = new Location(0f,0f);
+	}
+	
+	public Location getLocation(){
+		return currentLocation;
+	}
+	
+	public Location getVelocity(){
+		return velocity;
+	}
+	
+	public void updateLocation(Location loc){
+		currentLocation = loc;
+	}
+	
+	public void updateVelocity(Location vel){
+		velocity = vel;
+	}
+}
