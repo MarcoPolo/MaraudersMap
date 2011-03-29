94426a9374cef0cb2dfad786d03c552e95df1f3e
Marco Munizaga <Marco.emunizaga@gmail.com>
2011-03-29 09:01:23 -0400

    added files

--- GSF/src/loltest.java
+++ GSF/src/loltest.java
@@ -1,12 +1,25 @@
+import consolePrint.CartPoint;
+import consolePrint.Nodes;
+
 
 public class loltest {
 	public static void main(String[] args){
-		System.out.println("this is a test");
-		StringBuilder test = new StringBuilder("o hai there");
+//		System.out.println("this is a test");
+//		StringBuilder test = new StringBuilder("o hai there");
+//		System.out.println(test);
+//		test.insert(0, 'l');
+//		char a = test.charAt(0);
+//		System.out.println(a);
+		Nodes aNode = new Nodes(new CartPoint(0,0));
+		aNode.stackNodeOnTop(new CartPoint(2, 3),aNode);
+		aNode.stackNodeOnTop(new CartPoint(10, 9),aNode);
+		aNode.stackNodeOnTop(new CartPoint(21, 3),aNode);
+		aNode.printNodes(aNode);
+		CartPoint test = aNode.returnFirstPoint();
 		System.out.println(test);
-		int a = 2;
-		test.deleteCharAt(a);
-		test.insert(a, 'l');
-		System.out.println(test.toString());
+		aNode=aNode.popPoint();
+		aNode.printNodes(aNode);
+		
 	}
+	
 }
